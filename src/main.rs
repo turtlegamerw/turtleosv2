2207123968874959fdb40cb10c99386c560807cf
Turtlegamer_ <popperex9@gmail.com>
2024-04-13 16:44:04 +0200

    added smol stufffffffffffffffffffffffffffffffffffffffff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
